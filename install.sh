b0bb2ef928f4654d7a7931f0f55035f412a38e5b
Bartosz Szczeciński <54596452+bartko4321@users.noreply.github.com>
2026-08-01 18:17:30 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -101,7 +101,7 @@ sudo -v
 echo "$CURRENT_USER ALL=(ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/99-temp-installer > /dev/null
 
 # ── Usuwanie niechcianych pakietów ────────────────────────────
-PACKAGES_TO_REMOVE="htop nano plasma-browser-integration plasma-vault konqueror krdp krfb plasma-thunderbolt gnome-software epiphany decibels rhythmbox showtime cosmic-store cosmic-player parole"
+PACKAGES_TO_REMOVE="htop nano konqueror plasma-browser-integration plasma-vault krdp krfb plasma-thunderbolt kontact kmail kontrast plasma-welcome imagemagick kaddressbook kdepim-runtime akonadi-server akregator korganizer gnome-software epiphany decibels rhythmbox showtime cosmic-store cosmic-player parole"
 
 INSTALLED_PACKAGES=$(pacman -Qq $PACKAGES_TO_REMOVE 2>/dev/null || true)
 
